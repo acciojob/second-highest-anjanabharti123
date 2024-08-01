c280323b56e2a3132e26e0ebf6bce91568ae1430
Anjana Bharti <143540468+anjanabharti123@users.noreply.github.com>
2024-08-01 16:19:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length < 2) return -Infinity;
+
+    let highest = -Infinity;
+    let secondHighest = -Infinity;
+
+    for (const num of arr) {
+        if (num > highest) {
+            secondHighest = highest;
+            highest = num;
+        } else if (num > secondHighest && num < highest) {
+            secondHighest = num;
+        }
+    }
+
+    return secondHighest === -Infinity ? -Infinity : secondHighest;
+}
